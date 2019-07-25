a53968820590a5c60d5f1d37e2bcbd3598f84248
WEight22 <vul1543@gmail.com>
2019-07-25 23:30:19 +0900

    2nd commit

--- scripts/BeforeInstall.sh
+++ scripts/BeforeInstall.sh
@@ -2,10 +2,10 @@
 
 $(aws ecr get-login --no-include-email --registry-ids 129729052534 --region ap-northeast-1)
 
-if [ `docker images | grep tibami-student16 | wc -l`  = 1 ]
+if [ `docker images | grep tibame-student16 | wc -l`  = 1 ]
 then
-        docker rmi tibami-student16
-        docker pull 129729052534.dkr.ecr.ap-northeast-1.amazonaws.com/tibami-student16:latest
+        docker rmi tibame-student16
+        docker pull 129729052534.dkr.ecr.ap-northeast-1.amazonaws.com/tibame-student16:latest
 else
-        docker pull 129729052534.dkr.ecr.ap-northeast-1.amazonaws.com/tibami-student16:latest
+        docker pull 129729052534.dkr.ecr.ap-northeast-1.amazonaws.com/tibame-student16:latest
 fi
